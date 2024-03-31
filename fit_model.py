c95011e92157a6ff2588463b54a29efc22aa8221
Peter Fiee <peterfiee@icloud.com>
2024-03-31 07:46:33 -0400

    remove unnecessary data loading function. simplify logic

--- fit_model.py
+++ fit_model.py
@@ -1,22 +1,17 @@
 import json
 import os
 import pandas as pd
-from joblib import dump, load
+from joblib import dump
 from model import create_model
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 
 
-def load_dataset() -> pd.DataFrame:
+def fit_model() -> Pipeline:
     df: pd.DataFrame = pd.read_parquet("clean_data.parquet")
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_train, _, y_train, _ = train_test_split(
         df.drop(columns=["delay"]), df["delay"], test_size=0.2, random_state=42
     )
-    return X_train, X_test, y_train, y_test
-
-
-def fit_model() -> Pipeline:
-    X_train, _, y_train, _ = load_dataset()
 
     with open("hyperparams.json", "r") as f:
         hyperparameters: dict = json.load(f)
